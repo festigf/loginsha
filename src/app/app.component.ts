aca51649a9d0a4145d2b2e4885a8eea0a6bd5ce5
festigf <gianfranco.festi@marconi.loc>
2018-05-25 06:25:53 +0200

    Implementazione base utilizzo SHA3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { sha3_512 } from 'js-sha3';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  hash: String;
+  login: string;
+
+  constructor() {
+
+     this.hash= sha3_512("stringa");
+     console.log(this.hash);
+     
+  }
+
+  onClick(){
+
+    console.log(this.login);
+    this.hash= sha3_512(this.login);
+    console.log(this.hash);
+  }
 }
